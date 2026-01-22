0f0a985abaa86f29be602e0aab281567ab7ed0c2
sshajitha <shajithanazzer@gmail.com>
2026-01-22 11:59:52 +0530

    filename changed

--- admin-frontend/vite-project/src/main.jsx
+++ admin-frontend/vite-project/src/main.jsx
@@ -2,7 +2,7 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app.jsx'
+import App from './fapp.jsx'
 import "./app.css";
 
 createRoot(document.getElementById('root')).render(
