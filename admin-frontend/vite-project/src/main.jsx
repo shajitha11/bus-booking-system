9549bbf19fd8bb655e0473ba74e4cf67b90af2f3
sshajitha <shajithanazzer@gmail.com>
2026-01-22 11:52:06 +0530

    modified

--- admin-frontend/vite-project/src/main.jsx
+++ admin-frontend/vite-project/src/main.jsx
@@ -2,7 +2,7 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import "./app.css";
 
 createRoot(document.getElementById('root')).render(
